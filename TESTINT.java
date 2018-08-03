dbf33281225fdb6217fc9923e7f4052a4848b02e
Sarthak <sarthakbhutani.work@gmail.com>
2018-08-03 18:59:06 +0530

    Added TESTINT.java

---
+++ TESTINT.java
@@ -0,0 +1,14 @@
+package spoj;
+
+import java.util.Scanner;
+
+public class TESTINT {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int no1 = sc.nextInt();
+		int no2 = sc.nextInt();
+		int no3 = no1 + no2;
+		System.out.println(no3);
+	}
+
+}
