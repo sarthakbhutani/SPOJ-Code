dc928183e3c964d90062cc439a3bab4808bff047
Sarthak <sarthakbhutani.work@gmail.com>
2018-08-03 18:55:05 +0530

    Author: Sarthak Files Added: spoj Description : Added directory

--- Ch1.java
+++ Ch1.java
@@ -1,31 +1,22 @@
-package spoj;
-
 import java.util.Scanner;
  
-public class Ch1{
- 
+class Ch1{
+	 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int arr[] = new int[5];
+		int arr[] = new int[100];
 		int flag =0;
 		int count =0;
-		for(int i=0; i<5;i++) {
+		for(int i=0; i<10;i++) {
 		arr[i] = sc.nextInt();
 		count++;
 		if(arr[i]==42) {
 		flag =1;
-//		break;
+		break;
 		}
-		if(count ==0) {
+		if(flag==0) {
 			System.out.println( arr[i]);
 		}
-// 
-		
 		}
-		for(int i=0; i<count;i++) {
-		do {
-		System.out.println(arr[i]);
-		}while(arr[i]!=42);
 	}
-}
 }
